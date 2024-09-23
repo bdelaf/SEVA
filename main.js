f4473a5730e345026b72ae6dd42d8abca8ae2ca3
Thiago <thiagofaur0@gmail.com>
2024-09-23 10:02:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function () {
     return re.test(String(email).toLowerCase());
   }
   function validatePassword(password) {
-    const re = /^[a-zA-Z0-9._-]$/;
+    const re = /^[a-zA-Z0-9._-];
     return re.test(String(password));
   }
 });
