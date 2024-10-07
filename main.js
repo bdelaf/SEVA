8253d39b557237e0571b46e545221fff9a22e9d7
bdelaf <107963950+bdelaf@users.noreply.github.com>
2024-10-07 00:41:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -293,23 +293,19 @@ document.querySelector(".forgot-password")
     }
   });
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Redirección para el botón de Alumno
-  document.getElementById("btn-log_in-a").addEventListener("click", function () {
-    window.location.href = "login-a.html";  // Redirige a login de Alumno
-  });
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Redirección para el botón de Alumno
+//   document.getElementById("btn-log_in-a").addEventListener("click", function () {
+//     window.location.href = "login-a.html";  // Redirige a login de Alumno
+//   });
   
-    // Redirección para el botón de Profesor
-  document.getElementById("btn-log_in-prof").addEventListener("click", function () {
-    window.location.href = "profe-login.html";  // Redirige a login de Profesor
-  });
+//     // Redirección para el botón de Profesor
+//   document.getElementById("btn-log_in-prof").addEventListener("click", function () {
+//     window.location.href = "profe-login.html";  // Redirige a login de Profesor
+//   });
   
-    // Redirección para el botón de Administrador
-  document.getElementById("btn-log_in-adm").addEventListener("click", function () {
-    window.location.href = "login-adm.html";  // Redirige a login de Administrador
-  });
-});
-  
-window.onload = function() {
-  console.log("El DOM está completamente cargado.");
-};
+//     // Redirección para el botón de Administrador
+//   document.getElementById("btn-log_in-adm").addEventListener("click", function () {
+//     window.location.href = "login-adm.html";  // Redirige a login de Administrador
+//   });
+// });
