b329cf3346fed6f57a0d5d6af371917086826a47
bdela <bauti.cmb@gmail.com>
2024-10-14 22:27:14 -0300

    inscribirse.js terminado

--- inscribirse.js
+++ inscribirse.js
@@ -1,26 +1,39 @@
 function checkInscri() {
   var botones_inscri = document.getElementsByClassName("inscribirse active");
-
   for (var i = 0; i < botones_inscri.length; i++) {
     (function(boton) {
       boton.addEventListener("click", function () {
-        var confirmacion = confirm("¿Estás seguro que quieres inscribirte a esta comisión?");
-        
-        if (confirmacion) {
-          boton.innerHTML = "✔ Inscrito";
-          boton.style.color = "white";
-          boton.style.backgroundColor = "green";
-          boton.classList.remove("active");
+        if (boton.innerHTML === "Inscribirse") {
+          var confirmacion = confirm("¿Estás seguro que quieres inscribirte a esta comisión?");
+          if (confirmacion) {
+            boton.innerHTML = "❌ Darse de baja";
+            boton.style.color = "white";
+            boton.style.backgroundColor = "red";
+            alert("Te has inscrito exitosamente.");
 
-          var spanEstudiantes = boton.parentElement.querySelector('.students');
-          
-          var inscritos_total = spanEstudiantes.innerText.split('/');
-          var inscritos = parseInt(inscritos_total[0]);
-          var total = parseInt(inscritos_total[1]);
+            var estudiantes = boton.parentElement.querySelector('.students');
+            var inscritos_total = estudiantes.innerText.split('/');
+            var inscritos = parseInt(inscritos_total[0]);
+            var total = parseInt(inscritos_total[1]);
 
-          if (inscritos < total) {
-            inscritos += 1;
-            spanEstudiantes.innerText = inscritos + "/" + total;
+            if (inscritos < total) {
+              inscritos += 1;
+              estudiantes.innerText = inscritos + "/" + total;
+            }
+          }
+        } else if (boton.innerHTML === "❌ Darse de baja") {
+          var confirmacionBaja = confirm("¿Estás seguro que quieres darte de baja de esta comisión?");
+          if (confirmacionBaja) {
+            boton.innerHTML = "Inscribirse";
+            boton.style.color = "";
+            boton.style.backgroundColor = "";
+            alert("Se ha dado de baja exitosamente.");
+
+            var estudiantes = boton.parentElement.querySelector('.students');
+            var inscritos_total = estudiantes.innerText.split('/');
+            var inscritos = parseInt(inscritos_total[0]);
+            inscritos -= 1;
+            estudiantes.innerText = inscritos + "/" + inscritos_total[1];
           }
         }
       });
