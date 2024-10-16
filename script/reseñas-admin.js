b2e7eb6fe51bd298fce657f85ac980ca2a4ea9c1
santinohicke <santinohickethier@gmail.com>
2024-10-15 23:41:13 -0300

    Update reseñas-admin.js

--- script/reseñas-admin.js
+++ script/reseñas-admin.js
@@ -1,25 +1,39 @@
 // ADMINISTRADOR : ELIMINAR COMENTARIOS //
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", comentarios);
+
+function comentarios() {
   var checks = document.querySelectorAll('.check-c-adm');
-  var deleteLinks = document.querySelectorAll('.elim-com');
+  var EliminarCom = document.querySelectorAll('.elim-com');
 
+  asignarEventosChecks(checks);
+  asignarEventosEliminar(EliminarCom);
+}
+
+function asignarEventosChecks(checks) {
   for (var i = 0; i < checks.length; i++) {
-      checks[i].addEventListener('click', function() {
-          if (confirm('¿Quieres verificar este comentario?')) {
-              this.classList.toggle('active');
-          }
-      });
+    checks[i].addEventListener('click', verificarComentario);
   }
+}
 
-  for (var j = 0; j < deleteLinks.length; j++){
-      deleteLinks[j].addEventListener('click', function(event) {
-          event.preventDefault();
-          if (confirm('¿Estás seguro de que quieres eliminar este comentario?')) {
-              var review = this.closest('.review-c');
-              if (review) {
-                  review.remove();
-              }
-          }
-      });
-    }})
+function verificarComentario() {
+  if (confirm('¿Quieres verificar este comentario?')) {
+    this.classList.toggle('active');
+  }
+}
+
+function asignarEventosEliminar(EliminarCom) {
+  for (var j = 0; j < EliminarCom.length; j++) {
+    EliminarCom[j].addEventListener('click', eliminarComentario);
+  }
+}
+
+function eliminarComentario(event) {
+  event.preventDefault();
+  if (confirm('¿Estás seguro de que quieres eliminar este comentario?')) {
+    var review = this.closest('.review-c');
+    if (review) {
+      review.remove();
+    }
+  }
+}
