26d77a75fc5ebcb0df70a3f8ba608ec86d84b6d1
santinohicke <santinohickethier@gmail.com>
2024-10-20 04:18:40 -0300

    Update profe-sel.js

--- script/profe-sel.js
+++ script/profe-sel.js
@@ -1,46 +1,54 @@
 var tiemporestante;
 var contador;
 
+document.getElementById("materia-select").addEventListener("change", mostrarMateria);
+document.getElementById("alargar-reserva").addEventListener("click", mostrarSelectorHoras);
+document.getElementById("confirmar-alargue").addEventListener("click", confirmarAlargue);
+document.getElementById("cancelar-reserva").addEventListener("click", cancelarReserva);
 
-function ContFin () {
-    return alert("El tiempo a terminado")
-};
+function ContFin() {
+    alert("El tiempo ha terminado");
+}
 
-
-function temporizador (tiempo) {
-    tiempoRestante = tiempo * 60; // Convertir horas a segundos
+function temporizador(tiempo) {
+    tiemporestante = tiempo * 60; // Convertir horas a segundos
     var contenedor = document.getElementById('tiempo-restante');
-   
-    function actualizar () {
-        if (tiempoRestante <= 0){
-            return ContFin ();
-        }
-        var minutos = Math.floor(tiempoRestante / 60);
-        var segundos = tiempoRestante % 60;
+    actualizar(contenedor);
+}
 
-        contenedor.innerText = minutos + " : " + (segundos < 10 ? '0' : '') + segundos;
-        
-        tiempoRestante--;
-        
-        timer = setTimeout(actualizar, 1000);
-    
+function actualizar(contenedor) {
+    if (tiemporestante <= 0) {
+        return ContFin();
     }
-    actualizar() 
-};  
+    var minutos = Math.floor(tiemporestante / 60);
+    var segundos = tiemporestante % 60;
 
-document.getElementById("alargar-reserva").addEventListener("click", function () {
-    document.getElementById("selector-horas").style.display = "block";
-});
-
-document.getElementById("confirmar-alargue").addEventListener("click", function () {
-    var horasSeleccionadas = parseInt(document.getElementById("horas").value);
+    contenedor.innerText = minutos + " : " + (segundos < 10 ? '0' : '') + segundos;
     
+    tiemporestante--;
+    
+    timer = setTimeout(function() { actualizar(contenedor); }, 1000);
+}
+
+function mostrarMateria() {
+    var select = document.getElementById("materia-select");
+    var materiaNombre = document.getElementById("materia-nombre");
+    materiaNombre.textContent = select.value ? select.value : "Ninguna";
+}
+
+function mostrarSelectorHoras() {
+    document.getElementById("selector-horas").style.display = "block";
+}
+
+function confirmarAlargue() {
+    var horasSeleccionadas = parseInt(document.getElementById("horas").value);
     temporizador(horasSeleccionadas); 
     document.getElementById("selector-horas").style.display = "none"; 
-});
+}
 
-document.getElementById("cancelar-reserva").addEventListener("click", function(){
-    clearTimeout(timer)
+function cancelarReserva() {
+    clearTimeout(timer);
     document.getElementById('tiempo-restante').innerText = "00 : 00"; 
     alert('Reserva cancelada'); 
-});
+}
+
